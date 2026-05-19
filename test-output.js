c89286abc29065c275a60dc6221b43bef025e04e
hy123695044-cpu <hy123695044-cpu@users.noreply.github.com>
2026-05-19 10:06:37 +0800

    cleanup: remove dead map code, build artifacts, unused images

--- test-output.js
+++
@@ -1,5 +0,0 @@
-window.CONTENT = {
-  siteName: "兵姐康养旅居",
-  tagline: "后半生的另一种活法",
-  location: "浙江 · 安吉"
-};
--- write-html.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-// Load HTML template
-const html = require('./template.js');
-fs.writeFileSync('C:/Users/Administrator/morph-landing/index.html', html);
-console.log('Written OK');
